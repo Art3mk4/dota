adc4aeba7db85f01d2758d7d08c2939b721b055e
art3mk4 <art3mk4@gmail.com>
2016-05-11 14:06:13 +0600

    buy page

--- wp-content/themes/dota/page-buy-now.php
+++ wp-content/themes/dota/page-buy-now.php
@@ -54,7 +54,69 @@ get_header(); ?>
     <div class="container">
         <div class="row">
             <div class="col-lg-6 col-centered">
-                
+                <div id="can-text" class="color-white">
+                    You can register on one of the following betting services 
+                        and start betting right now!
+                </div>
+            </div>
+            <div class="col-lg-8 col-centered">
+                <div class="odd-service display-table-cell row">
+                    <div class="logo">
+                        <img class="img-responsive" src="<?php echo get_template_directory_uri()?>/images/-15-.png" />
+                    </div>
+                    <div class="service-text color-white">
+                        Pinnacle - the most popular betting office
+                    </div>
+                    <div class="btn-wrapper pulll-right">
+                        <a class="btn register-button color-white">Register Now</a>
+                    </div>
+                </div>
+                <div class="even-service display-table-cell">
+                    <div class="logo">
+                            <img class="img-responsive" src="<?php echo get_template_directory_uri()?>/images/-16.png" />
+                        </div>
+                        <div class="service-text color-white">
+                            Betway - one of the biggest European betting offices 
+    for eSports and poker
+                        </div>
+                    <div class="btn-wrapper">
+                        <a class="btn register-button color-white">Register Now</a>
+                    </div>
+                </div>
+                <div class="odd-service display-table-cell">
+                    <div class="logo">
+                        <img class="img-responsive" src="<?php echo get_template_directory_uri()?>/images/-17.png" />
+                    </div>
+                    <div class="service-text color-white">
+                        Egamingbets - specializes in eSports betting
+                    </div>
+                    <div class="btn-wrapper">
+                        <a class="btn register-button color-white">Register Now</a>
+                    </div>
+                </div>
+                <div class="even-service display-table-cell">
+                    <div class="logo">
+                        <img class="img-responsive" src="<?php echo get_template_directory_uri()?>/images/-18.png" />
+                    </div>
+                    <div class="service-text color-white">
+                        Bet365 - this UK service currently has more 
+than 18 million registered users
+                    </div>
+                    <div class="btn-wrapper">
+                        <a class="btn register-button color-white">Register Now</a>
+                    </div>
+                </div>
+                <div class="odd-service display-table-cell">
+                    <div class="logo">
+                        <img class="img-responsive" src="<?php echo get_template_directory_uri()?>/images/-19.png" />
+                    </div>
+                    <div class="service-text color-white">
+                        William Hill - now it is one of the biggest betting offices in the UK
+                    </div>
+                    <div class="btn-wrapper">
+                        <a class="btn register-button color-white">Register Now</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
