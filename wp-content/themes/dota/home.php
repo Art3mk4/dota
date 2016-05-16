e90f509bc4ded458cac8bccf7e05d87a04f83950
art3mk4 <art3mk4@gmail.com>
2016-05-16 16:24:36 +0600

    image resize, add home page

--- wp-content/themes/dota/home.php
+++ wp-content/themes/dota/home.php
@@ -7,7 +7,9 @@ $guide = get_site_option('wppay_guide_setting');
             <div class="row">
                 <div class="col-xs-12">
                     <div id="betting" class="text-center">
+                    	<a href="<?php echo esc_url( home_url('/'));?>">
                         <img class="img-responsive center-block" src="<?php echo get_template_directory_uri()?>/images/esport-betting.png" />
+                        </a>
                         <!--h1 class="color-white defused">
                             e<span class="defused-bold">S</span>port<span class="color-orange">&nbsp<span class="defused-bold">B</span>etting</span>
                         </h1-->
--- wp-content/themes/dota/page-buy-now.php
+++ wp-content/themes/dota/page-buy-now.php
@@ -6,7 +6,9 @@ get_header();
         <div class="row">
             <div class="col-xs-12">
                 <div id="betting">
+                    <a href="<?php echo esc_url( home_url('/') )?>">
                     <img class="img-responsive center-block" src="<?php echo get_template_directory_uri()?>/images/esport-betting.png" />
+                    </a>
                 </div>
             </div>
         </div>
